95eeb688393adfb3b5eb836221671e8948f0c92a
goxp49 <215920363@qq.com>
2018-11-08 19:02:45 +0800

    添加Key部分，移除main.c，整合到app.c中

--- APP/app.c
+++ APP/app.c
@@ -12,6 +12,38 @@
 
 #include "includes.h"
 
+OS_SEM SemOfKey;          //标志KEY1是否被单击的多值信号量
+
+int main(void)
+{
+
+	OS_ERR err;
+	BSP_Init();			//板级初始化
+	CPU_Init();			//CPU初始化
+	OSInit(&err);		//系统初始化
+	Mem_Init();
+
+	/*创建任务*/
+
+	 OSTaskCreate((OS_TCB	  *)&TIMER_TCB,	   //任务控制块指针
+					  (CPU_CHAR   *)"LED2", 	  //任务名称
+					  (OS_TASK_PTR )Task_TIMER,	  //任务代码指针
+					  (void 	  *)0,			  //传递给任务的参数parg
+					  (OS_PRIO	   )3,			  //任务优先级
+					  (CPU_STK	  *)&TIMER_Stk[0],	  //任务堆栈基地址
+					  (CPU_STK_SIZE)12, 		  //堆栈剩余警戒线
+					  (CPU_STK_SIZE)128,		  //堆栈大小
+					  (OS_MSG_QTY  )0,			  //可接收的最大消息队列数
+					  (OS_TICK	   )0,			  //时间片轮转时间
+					  (void 	  *)0,			  //任务控制块扩展信息
+					  (OS_OPT	   )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),  //任务选项
+					  (OS_ERR	  *)&err);		  //返回值
+
+    OSStart(&err);
+
+
+}
+
 /*
  * 函数名：Task_LED
  * 描述  : LED流水灯
@@ -45,7 +77,7 @@ void Task_LED2(void *p_arg)
 
     }
 }
-
+//-------------------------------------------------------------------------------------------------------
 void Task_TIMER(void *p_arg)
 {
 	OS_ERR err;
@@ -77,6 +109,27 @@ void TmrCallBack(OS_TMR *p_tmr, void *p_arg)
 	//CPU_SR_ALLOC();
 
 	LED1_Switch();
+
+}
+//-------------------------------------------------------------------------------------------------------
+
+void Task_KEY_SCAN(void *p_arg)
+{
+	OS_ERR err;
+	OS_TMR tmr;
+	uint8_t keyStatus;
+
+    (void)p_arg;                		// 'p_arg' 并没有用到，防止编译器提示警告
+
+	OSSemCreate((OS_SEM *) &SemOfKey,
+		(CPU_CHAR *) "key_scan",
+		(OS_SEM_CTR) 0,
+		(OS_ERR *) &err);
+
+	while(Key_Scan() == KEY_OFF)
+	{
+
+	};
 
 }
 
