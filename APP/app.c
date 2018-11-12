5d925be8f66b5b0daf26331f0678b814ab125455
goxp49 <215920363@qq.com>
2018-11-12 19:36:49 +0800

    正在添加信息队列例程

--- APP/app.c
+++ APP/app.c
@@ -12,12 +12,13 @@
 
 #include "includes.h"
 
-static OS_TCB SEM_KEY_TCB;
-static CPU_STK SEM_KEY_Stk[128];
+static OS_Q * p_q;
 
-static OS_TCB SEM_LED_TCB;
-static CPU_STK SEM_LED_Stk[128];
+static OS_TCB Q_POST_TCB;
+static CPU_STK Q_POST_Stk[128];
 
+static OS_TCB Q_PEND_TCB;
+static CPU_STK Q_PEND_Stk[128];
 
 static OS_SEM SemOfKey;          //标志KEY1是否被单击的多值信号量
 
@@ -29,21 +30,20 @@ int main(void)
 	CPU_Init();			//CPU初始化
 	OSInit(&err);		//系统初始化
 	Mem_Init();
-	
-	OSSemCreate((OS_SEM *) &SemOfKey,
-		(CPU_CHAR *) "key_scan",
-		(OS_SEM_CTR) 0,
-		(OS_ERR *) &err);
 
+	OSQCreate((OS_Q *) p_q,
+		(CPU_CHAR *) "queue",
+		(OS_MSG_QTY) 20,
+		(OS_ERR *) &err)
 
 	/*创建任务*/
 
-	 OSTaskCreate((OS_TCB *)&SEM_KEY_TCB,	   //任务控制块指针
+	 OSTaskCreate((OS_TCB *)&Q_POST_TCB,	   //任务控制块指针
 					  (CPU_CHAR   *)"KEY", 	  //任务名称
-					  (OS_TASK_PTR )Task_SEM_KEY_SCAN,	  //任务代码指针
+					  (OS_TASK_PTR )Task_QUEUE_POST,	  //任务代码指针
 					  (void 	  *)0,			  //传递给任务的参数parg
 					  (OS_PRIO	   )3,			  //任务优先级
-					  (CPU_STK	  *)&SEM_KEY_Stk[0],	  //任务堆栈基地址
+					  (CPU_STK	  *)&Q_POST_Stk[0],	  //任务堆栈基地址
 					  (CPU_STK_SIZE)12, 		  //堆栈剩余警戒线
 					  (CPU_STK_SIZE)128,		  //堆栈大小
 					  (OS_MSG_QTY  )5,			  //可接收的最大消息队列数
@@ -51,13 +51,13 @@ int main(void)
 					  (void 	  *)0,			  //任务控制块扩展信息
 					  (OS_OPT	   )(OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),  //任务选项
 					  (OS_ERR	  *)&err);		  //返回值
-					  
-	OSTaskCreate((OS_TCB *)&SEM_LED_TCB,	  //任务控制块指针
+
+	OSTaskCreate((OS_TCB *)&Q_PEND_TCB,	  //任务控制块指针
 						 (CPU_CHAR	 *)"LED",		 //任务名称
 						 (OS_TASK_PTR )Task_SEM_LED,	 //任务代码指针
 						 (void		 *)0,			 //传递给任务的参数parg
 						 (OS_PRIO	  )2,			 //任务优先级
-						 (CPU_STK	 *)&SEM_LED_Stk[0], 	 //任务堆栈基地址
+						 (CPU_STK	 *)&Q_PEND_Stk[0], 	 //任务堆栈基地址
 						 (CPU_STK_SIZE)12,			 //堆栈剩余警戒线
 						 (CPU_STK_SIZE)128, 		 //堆栈大小
 						 (OS_MSG_QTY  )5,			 //可接收的最大消息队列数
@@ -72,6 +72,41 @@ int main(void)
 }
 
 
+void Task_QUEUE_POST(void *p_arg)
+{
+	OS_ERR err;
+    (void)p_arg;                		// 'p_arg' 并没有用到，防止编译器提示警告
+
+
+	while(1)
+	{
+		OSQPost((OS_Q *) p_q,
+			(void *) "Hellow",
+			(OS_MSG_SIZE) sizeof("Hellow"),
+			(OS_OPT)  OS_OPT_POST_FIFO | OS_OPT_POST_ALL,
+			(OS_ERR) * err)
+
+		OSTimeDlyHMSM( 0, 0, 0, 500, OS_OPT_TIME_PERIODIC, &err);
+	};
+
+}
+
+void Task_QUEUE_PEND(void *p_arg)
+{
+	OS_ERR err;
+    (void)p_arg;                		// 'p_arg' 并没有用到，防止编译器提示警告
+
+
+	while(1)
+	{
+		os
+	};
+
+}
+
+
+#if(0)
+
 void Task_SEM_KEY_SCAN(void *p_arg)
 {
 	OS_ERR err;
@@ -79,7 +114,7 @@ void Task_SEM_KEY_SCAN(void *p_arg)
     (void)p_arg;                		// 'p_arg' 并没有用到，防止编译器提示警告
 
 	while(1)
-	{		
+	{
 		if(Key_Scan(GPIOA, GPIO_Pin_0) == KEY_ON)
 		{
 			OSSemPost(&SemOfKey, OS_OPT_POST_ALL, &err);
@@ -96,14 +131,14 @@ void Task_SEM_LED(void *p_arg)
     (void)p_arg;
 
 	while(1)
-	{		
+	{
 		OSSemPend(&SemOfKey, 0, OS_OPT_PEND_BLOCKING, (CPU_TS*)0, &err);
 		LED1_Switch();
 	};
 
 }
 
-
+#endif
 //-------------------------------------------------------------------------------------------------------
 
 void Task_LED1(void *p_arg)
